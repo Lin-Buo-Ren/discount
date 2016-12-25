32a50d1f84e3a179cc2362463a2975d164afefe1
林博仁 <Buo.Ren.Lin@gmail.com>
2016-12-26 07:54:34 +0800

    Support <h1> title parsing
    Refer-to: #155
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

--- mkdio.c
+++ mkdio.c
@@ -358,3 +358,35 @@ mkd_ref_prefix(Document *f, char *data)
     if ( f )
 	f->ref_prefix = data;
 }
+
+Paragraph *
+mkd_h1(Paragraph *p)
+{
+    Paragraph *found;
+
+    while ( p ) {
+        if ( p->typ == HDR && p->hnumber == 1 )
+             return p;
+       if ( p->down && (found = mkd_h1(p->down)) )
+           return found;
+        p = p->next;
+    }
+    return 0;
+}
+
+char *
+mkd_h1_title(Document *doc, int flags)
+{
+    Paragraph *title;
+
+    if (doc && (title = mkd_h1(doc->code)) ) {
+          char *generated;
+          int size;
+
+          /* assert that a H1 header is one line long, so that's the only thing needed */
+          size = mkd_line(T(title->text->text), S(title->text->text), &generated, flags);
+          if ( size ) return generated;
+    }
+    return 0;
+}
+
